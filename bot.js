1b896f60f57555918414b1d626501122b48bd7ee
Selina Liu <liu15@seas.upenn.edu>
2016-07-20 00:02:05 -0400

    add quote functionality

--- bot.js
+++ bot.js
@@ -1,14 +1,31 @@
 var token = "217076975:AAGu6shAF25MrRZvCCiX2fALTJk1cbLU-2E";
 
 var Bot = require('node-telegram-bot-api');
+var unirest = require('unirest');
 var bot = new Bot(token, {polling: true});
 
 console.log('bot server started...');
 
 bot.onText(/^\/say_hello(.+)$/, function (msg, match) {
     var name = match[1];
-    bot.sendMessage(msg.chat.id, 'Hello ' + name + ', good morning!')
+    var string = '\n What are you up to today?';
+    bot.sendMessage(msg.chat.id, 'Sup' + name + string)
     .then(function () {
         // reply sent!
+    });
+});
+
+bot.onText(/^\/quote$/, function (msg, match) {
+    unirest.post("https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous")
+    .header("X-Mashape-Key", "zBULkZgMzamshDNXKLHPRvoNv7ipp1haR75jsnIhDyzxLGjO7w")
+    .header("Content-Type", "application/x-www-form-urlencoded")
+    .header("Accept", "application/json")
+    .end(function (result) {
+          console.log(result.status, result.headers, result.body);
+          var data = JSON.parse(result.body);
+          console.log("the quote: " + data.quote);
+          bot.sendMessage(msg.chat.id, data.quote).then(function () {
+              // reply sent!
+          });
     });
 });
